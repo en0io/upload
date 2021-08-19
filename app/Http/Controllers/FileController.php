14769dbb9f1a25478815ca23c33dd01836ab8859
Alyx Wijers <akw@en0.io>
2021-08-19 15:59:47 -0700

    added support css for frontend

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function processUpload(Request $request)
+    {
+
+    }
+    public function
+}
