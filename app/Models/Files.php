f260682cdce90e308d86ebd0734dd363c492117f
Alyx Wijers <akw@en0.io>
2021-08-19 16:06:52 -0700

    Added relationships to models

--- app/Models/Files.php
+++ app/Models/Files.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Files extends Model
 {
     use HasFactory;
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function files()
+    {
+        return $this->hasMany(Files::class);
+    }
 }
