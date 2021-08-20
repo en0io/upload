5951d6f1bad6d28c514361daaeafe2b114e1aefb
Alyx Wijers <akw@en0.io>
2021-08-20 11:51:39 -0700

    added softdeletes

--- app/Models/Files.php
+++ app/Models/Files.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Files extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     public function owner()
     {
         return $this->belongsTo(User::class);
